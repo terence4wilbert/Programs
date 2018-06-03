de7a6037d94004a28589dfc5ab161711d0901371
terence4wilbert <terence4wilbert@gmail.com>
2018-06-02 20:14:48 -0500

    GO System programming

---
+++ GO/systemGO/test.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func multipleOfIndex(b []int) [ ]int{
+    return b
+}
+
+func main() {
+    b :=[ ]int{22,-6,32,82,9,25}
+    var answer =  multipleOfIndex(b)
+    fmt.Println(answer)
+}
